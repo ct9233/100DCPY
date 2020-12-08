88961e54fc76e2a55d78a7b9ff1de10f28b7b5c1
Chris Twaddell <ct9233@gmail.com>
2020-12-07 22:33:25 -0800

    ASCII Art and Game Progression

--- 07 - Hangman/hangman.py
+++ 07 - Hangman/hangman.py
@@ -1,22 +1,98 @@
 import random
 
+stages = [
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""",
+    """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+""",
+]
 word_list = ["aardvark", "barracuda", "cheetah"]
 chosen_word = random.choice(word_list)
 
+lives = 6
+game_end = False
 display = []
+
 for letter in chosen_word:
     display.append("_")
-print(display)
 
-while "_" in display:
+print("Welcome to Hangman!")
+print(stages[lives])
+print(f"{' '.join(display)}\n")
+
+while not game_end:
     guess = input("Guess a letter: ").lower()
 
     for position in range(len(chosen_word)):
         if chosen_word[position] == guess:
-            print("Right")
             display[position] = guess
-        else:
-            print("Wrong")
-    print(display)
+    if guess not in display:
+        lives -= 1
+        if lives == 0:
+                game_end = True
+                print("You lose.")
 
-print("You win!")
+    if "_" not in display:
+        game_end = True
+        print("You win!")
+
+    print(stages[lives])
+    print(f"{' '.join(display)}\n")
