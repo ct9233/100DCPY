43d21dc828ad53a24b4987f87efd540086bfdb83
Chris Twaddell <ct9233@gmail.com>
2020-12-08 19:40:06 -0800

    Parameter Differentiation

--- 08 - Caesar Cipher/caesar_cipher.py
+++ 08 - Caesar Cipher/caesar_cipher.py
@@ -32,17 +32,17 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(text, shift):
+def encrypt(original_text, shift_amount):
     original_indexes = []
     encrypted_indexes = []
     encrypted_text = []
-    for letter in text:
+    for letter in original_text:
         original_indexes.append(alphabet.index(letter))
     for index in original_indexes:
-        if index + shift < 25:
-            encrypted_indexes.append(index + shift)
+        if index + shift_amount < 25:
+            encrypted_indexes.append(index + shift_amount)
         else:
-            encrypted_indexes.append(index + shift - 26)
+            encrypted_indexes.append(index + shift_amount - 26)
     for index in encrypted_indexes:
         encrypted_text.append(alphabet[index])
     print(f"The encrypted output is: {''.join(encrypted_text)}")
