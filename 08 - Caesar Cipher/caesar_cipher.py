098766c01af0fef56d1325ffb382e367a5e8746f
Chris Twaddell <ct9233@gmail.com>
2020-12-08 22:17:08 -0800

    Varied Input Handling and Art

--- 08 - Caesar Cipher/caesar_cipher.py
+++ 08 - Caesar Cipher/caesar_cipher.py
@@ -27,6 +27,10 @@ alphabet = [
     "z",
 ]
 
+from art import logo
+
+print(logo)
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
@@ -35,23 +39,34 @@ shift = int(input("Type the shift number:\n"))
 def caesar(original_text, shift_amount, encoding_direction):
     original_indexes = []
     transformed_indexes = []
-    returned_text = ""
+    returned_text = []
+    if shift_amount > 26:
+        shift_amount = shift_amount % 26
     for letter in original_text:
-        original_indexes.append(alphabet.index(letter))
+        if letter in alphabet:
+            original_indexes.append(alphabet.index(letter))
+        else:
+            original_indexes.append(letter)
     for index in original_indexes:
-        if encoding_direction == "encode":
-            if index + shift_amount < 25:
-                transformed_indexes.append(index + shift_amount)
-            else:
-                transformed_indexes.append(index + shift_amount - 26)
-        elif encoding_direction == "decode":
-            if index - shift_amount > 0:
-                transformed_indexes.append(index - shift_amount)
-            else:
-                transformed_indexes.append(index - shift_amount + 26)
+        if type(index) == int:
+            if encoding_direction == "encode":
+                if index + shift_amount < 25:
+                    transformed_indexes.append(index + shift_amount)
+                else:
+                    transformed_indexes.append(index + shift_amount - 26)
+            elif encoding_direction == "decode":
+                if index - shift_amount >= 0:
+                    transformed_indexes.append(index - shift_amount)
+                else:
+                    transformed_indexes.append(index - shift_amount + 26)
+        else:
+            transformed_indexes.append(index)
     for index in transformed_indexes:
-        returned_text += alphabet[index]
-    print(f"The {encoding_direction}d output is: {returned_text}")
+        if type(index) == int:
+            returned_text.append(alphabet[index])
+        else:
+            returned_text.append(index)
+    print(f"The {encoding_direction}d output is: {''.join(returned_text)}")
 
 
 caesar(text, shift, direction)
