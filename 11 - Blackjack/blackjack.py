fe6e41721b63b0899169523bf90c59c89d87763f
Chris Twaddell <ct9233@gmail.com>
2020-12-12 21:35:43 -0800

    Game Termination Fix

--- 11 - Blackjack/blackjack.py
+++ 11 - Blackjack/blackjack.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import random
 from art import logo
 
@@ -13,7 +14,14 @@ def blackjack_round():
 
     user_cards = []
     computer_cards = []
-    round_continues = True
+
+    def another_round():
+        play_again = input("Play again?  Type 'y' or 'n': ")
+        if play_again == "y":
+            blackjack_round()
+        else:
+            print("\nGoodbye!")
+            input("\nPress Enter to exit...")
 
     user_cards.append(random.choice(cards))
     user_cards.append(random.choice(cards))
@@ -21,6 +29,8 @@ def blackjack_round():
     computer_cards.append(random.choice(cards))
 
     print(f"\nComputer's first card: {computer_cards[0]}")
+
+    round_continues = True
 
     while round_continues:
         if sum(user_cards) == 21:
@@ -31,6 +41,7 @@ def blackjack_round():
             print(f"\nComputer cards: {computer_cards}.  Blackjack! Computer wins!\n")
             round_continues = False
             another_round()
+
         print(f"\nPlayers cards: {user_cards}, current score: {sum(user_cards)}")
         player_hit = input("Type 'y' to get another card, or type 'n' to hold: ")
         if player_hit == "y":
@@ -46,39 +57,33 @@ def blackjack_round():
                 print(f"\nComputer cards: {computer_cards}.  Score: {sum(computer_cards)}  Bust! Computer lost.\n")
                 round_continues = False
                 another_round()
-            else:
-                if sum(computer_cards) > sum(user_cards):
-                    print(
-                        f"\nComputer cards: {computer_cards}.  Score: {sum(computer_cards)}\nPlayer cards: {user_cards}.  Score: {sum(user_cards)}\nComputer wins!\n"
-                    )
-                    round_continues = False
-                    another_round()
-                elif sum(computer_cards) < sum(user_cards):
-                    print(
-                        f"\nComputer cards: {computer_cards}.  Score: {sum(computer_cards)}\nPlayer cards: {user_cards}.  Score: {sum(user_cards)}\nPlayer wins!\n"
-                    )
-                    round_continues = False
-                    another_round()
-                elif sum(computer_cards) == sum(user_cards):
-                    print(
-                        f"\nComputer cards: {computer_cards}.  Score: {sum(computer_cards)}\nPlayer cards: {user_cards}.  Score: {sum(user_cards)}\nIt's a draw!\n"
-                    )
-                    round_continues = False
-                    another_round()
-
-
-def another_round():
-    play_again = input("Play again?  Type 'y' or 'n': ")
-    if play_again == "y":
-        blackjack_round()
-    else:
-        print("\nGoodbye!")
+            elif sum(computer_cards) == 21:
+                print(f"\nComputer cards: {computer_cards}.  Score: {sum(computer_cards)}  Blackjack! Computer wins!\n")
+                round_continues = False
+                another_round()
+            elif sum(computer_cards) > sum(user_cards):
+                print(
+                    f"\nComputer cards: {computer_cards}.  Score: {sum(computer_cards)}\nPlayer cards: {user_cards}.  Score: {sum(user_cards)}\nComputer wins!\n"
+                )
+                round_continues = False
+                another_round()
+            elif sum(computer_cards) < sum(user_cards):
+                print(
+                    f"\nComputer cards: {computer_cards}.  Score: {sum(computer_cards)}\nPlayer cards: {user_cards}.  Score: {sum(user_cards)}\nPlayer wins!\n"
+                )
+                round_continues = False
+                another_round()
+            elif sum(computer_cards) == sum(user_cards):
+                print(
+                    f"\nComputer cards: {computer_cards}.  Score: {sum(computer_cards)}\nPlayer cards: {user_cards}.  Score: {sum(user_cards)}\nIt's a draw!\n"
+                )
+                round_continues = False
+                another_round()
 
 
 play_game = input("\nWould you like to start a round of Blackjack? (type 'y' or 'n'): ")
 
 if play_game == "y":
     blackjack_round()
-
-
-input("\nPress Enter to exit...")
+else:
+    input("\nPress Enter to exit...")
