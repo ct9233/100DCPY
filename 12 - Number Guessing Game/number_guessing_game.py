d07a68e8a5e272f5873c176c31c0fae7b4ee3f99
Chris Twaddell <ct9233@gmail.com>
2020-12-15 23:38:28 -0800

    Game Function Completed

--- 12 - Number Guessing Game/number_guessing_game.py
+++ 12 - Number Guessing Game/number_guessing_game.py
@@ -2,28 +2,32 @@ import random
 from art import logo
 
 print(logo)
-print("Welcome to the Number Guessing Game!\n")
+print("Welcome to the Number Guessing Game!\n\n")
 
-print("I'm thinking of a number between 1 and 100.")
+print("I'm thinking of a number between 1 and 100.\n")
 difficulty = input("Choose a difficulty.  Type 'easy' or 'hard': ")
 
 attempts = 10 if difficulty == "easy" else 5
 target_number = random.randint(1, 100)
 
+
 def evaluate_guess(current_guess):
-        if current_guess == target_number:
-            print(f"You got it! The answer was {target_number}.")
-        elif current_guess > target_number:
-            print("Too high.")
-        elif current_guess < target_number:
-            print("Too low")
+    if current_guess == target_number:
+        print(f"\nYou got it! The answer was {target_number}.")
+        return "win"
+    elif current_guess > target_number:
+        print("\nToo high.")
+    elif current_guess < target_number:
+        print("\nToo low")
+
 
 for i in range(attempts):
     player_guess = int(input("Make a guess: "))
-    evaluate_guess(player_guess)
-    if i < 5:
+    if evaluate_guess(player_guess) == "win":
+        break
+    if i < attempts - 1:
         print(f"You have {attempts - 1 - i} remaining to guess the number.")
-    
+    if i == attempts - 1:
+        print("You have run out of guesses.  Game over!\n")
 
-
-print("You have run out of guesses.  Game over!")
+input("Press Enter to exit...")
