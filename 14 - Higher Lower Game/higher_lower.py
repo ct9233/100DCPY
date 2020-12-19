b20ea347573e5ce40fa7f66504c8878b9ef74b58
Chris Twaddell <ct9233@gmail.com>
2020-12-18 17:11:40 -0800

    Initial Setup, Art and Data

---
+++ 14 - Higher Lower Game/higher_lower.py
@@ -0,0 +1,10 @@
+import random
+import os
+from art import logo, vs
+from game_data import data
+
+print(logo)
+print(f"Compare A: ")
+print(vs)
+print(f"Against B: ")
+input("Who has more followers? Type 'A' or 'B': ")
