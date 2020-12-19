18375abd364004909a5fecb24bdd4f97a1d3e415
Chris Twaddell <ct9233@gmail.com>
2020-12-18 19:32:26 -0800

    Random Data Selection and Propagation

--- 14 - Higher Lower Game/higher_lower.py
+++ 14 - Higher Lower Game/higher_lower.py
@@ -3,8 +3,22 @@ import os
 from art import logo, vs
 from game_data import data
 
-print(logo)
-print(f"Compare A: ")
-print(vs)
-print(f"Against B: ")
-input("Who has more followers? Type 'A' or 'B': ")
+game_continues = True
+compare_a = ""
+compare_b = ""
+index_a = int
+index_b = int
+
+while game_continues == True:
+    index_a = index_a if compare_a != "" else random.randrange(0, len(data))
+    index_b = random.randrange(0, len(data))
+    while index_b == index_a:
+            index_b = random.randrange(0, len(data))
+    compare_a = compare_a if compare_a != "" else data[index_a]
+    compare_b = data[index_b]
+
+    print(logo)
+    print(f"Compare A: {compare_a['name']}, a {compare_a['description']}, from {compare_a['country']}.")
+    print(vs)
+    print(f"Against B: {compare_b['name']}, a {compare_b['description']}, from {compare_b['country']}.")
+    player_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
