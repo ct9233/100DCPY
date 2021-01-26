95033f782532312e5a25202a111bb44c41b1a494
Chris Twaddell <ct9233@gmail.com>
2021-01-25 18:02:43 -0800

    Menu and Art

---
+++ 15 - Coffee Machine/coffee_machine.py
@@ -0,0 +1,14 @@
+import os
+from art import logo, priceboard
+from menu import MENU
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+print(logo)
+print(priceboard)
+
+input("Press enter to exit...")
