45e4eab35bb793f2368529f598c971a9e3980d22
Chris Twaddell <ct9233@gmail.com>
2021-01-30 17:59:06 -0800

    Added Art

--- 16 - OOP Coffee Machine/main.py
+++ 16 - OOP Coffee Machine/main.py
@@ -1,3 +1,11 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+from art import logo, priceboard
+
+
+print(logo)
+print(priceboard)
+
+
+
