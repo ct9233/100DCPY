9c9a56ef64735795d1f381d18c9aa0f5aedf590f
Chris Twaddell <ct9233@gmail.com>
2021-02-03 17:36:37 -0800

    Animating Snake Segments

--- 20 + 21 - Snake Game/main.py
+++ 20 + 21 - Snake Game/main.py
@@ -4,6 +4,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 segments = []
 
@@ -13,6 +14,17 @@ for i in range(3):
     segments[i].color("medium sea green")
     segments[i].penup()
     segments[i].setpos(-i * 20, 0)
+
+game_on = True
+
+while game_on:
+    screen.update()
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
 
 screen.exitonclick()
