fb197156912a566359a33f988852ef228bf985be
Chris Twaddell <ct9233@gmail.com>
2021-02-05 17:10:09 -0800

    Screen and Paddle

---
+++ 22 - Pong/main.py
@@ -0,0 +1,31 @@
+from turtle import Screen, Turtle
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+paddle = Turtle("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+def go_up():
+    new_y = paddle.ycor() + 28
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 28
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_on = True
+
+while game_on:
+    screen.update()
+
+screen.exitonclick()
