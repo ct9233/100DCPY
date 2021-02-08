ba90394549696314c03f975ff4fe35a9853c5b05
Chris Twaddell <ct9233@gmail.com>
2021-02-07 19:22:26 -0800

    Ball Out of Bounds and Reset

--- 22 - Pong/main.py
+++ 22 - Pong/main.py
@@ -28,6 +28,20 @@ while game_on:
     
     # Detect wall collision
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() > -320:
+        ball.bounce_x()
+
+    # Detect r_paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect l_paddle misses:
+    if ball.xcor() < -380:
+        ball.reset_position()
+
+
 
 screen.exitonclick()
