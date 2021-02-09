f938a507a6482cec67f773ed5fcf75284b4d9f02
Chris Twaddell <ct9233@gmail.com>
2021-02-08 17:56:19 -0800

    Car Manager and Car Movement

--- 23 - Turtle Crossing/car_manager.py
+++ 23 - Turtle Crossing/car_manager.py
@@ -1,7 +1,25 @@
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+from turtle import Turtle
+import random
+
+COLORS = ["crimson", "orange", "yellow", "cyan", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager():
+    def __init__(self):
+        self.cars = []
+
+    def create_car(self):
+        random_y = random.randrange(-240, 250)
+        new_car = Turtle("square")
+        new_car.color(random.choice(COLORS))
+        new_car.shapesize(stretch_len=2)
+        new_car.penup()
+        new_car.goto(320, random_y)
+        self.cars.append(new_car)
+
+    def move_cars(self):
+        for car in range(len(self.cars)):
+            new_x = self.cars[car].xcor() - STARTING_MOVE_DISTANCE
+            self.cars[car].goto(new_x, self.cars[car].ycor())
--- 23 - Turtle Crossing/main.py
+++ 23 - Turtle Crossing/main.py
@@ -10,11 +10,18 @@ screen.bgcolor("black")
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.move_up, "Up")
 
 game_is_on = True
+car_ticker = 0
 while game_is_on:
+    car_ticker += 1
     time.sleep(0.1)
     screen.update()
+    if car_ticker % 6 == 0:
+        car_ticker = 0
+        car_manager.create_car()
+    car_manager.move_cars()
