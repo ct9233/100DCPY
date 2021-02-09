514d6c8fe896db7cba3f424e903a4f1ddb055f40
Chris Twaddell <ct9233@gmail.com>
2021-02-08 16:06:42 -0800

    Player Class and Input

--- 23 - Turtle Crossing/main.py
+++ 23 - Turtle Crossing/main.py
@@ -9,6 +9,11 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
--- 23 - Turtle Crossing/player.py
+++ 23 - Turtle Crossing/player.py
@@ -1,7 +1,24 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("yellow green")
+        self.setheading(90)
+        self.penup()
+        self.set_start()
+
+    def set_start(self):
+        self.goto(STARTING_POSITION)
+
+    def move_up(self):
+        if self.ycor() < FINISH_LINE_Y:
+            self.goto(self.xcor(), self.ycor() + MOVE_DISTANCE)
+        else:
+            self.set_start()
