e8f056a04a3d13b18b040eb2f80f418ed3627404
Chris Twaddell <ct9233@gmail.com>
2021-02-11 21:24:26 -0800

    Letter Input and Output

--- 24 - Mail Merge Project/main.py
+++ 24 - Mail Merge Project/main.py
@@ -3,3 +3,16 @@
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
 
+with open("./Input/Names/invited_names.txt") as names:
+    invited_names = names.readlines()
+
+invited_names = [i[:-1] if "\n" in i else i for i in invited_names]
+
+
+for name in invited_names:
+    with open("./Input/Letters/starting_letter.txt") as letter:
+        form_letter = letter.readlines()
+    form_letter[0] = form_letter[0].replace("[name]", f"{name}")
+    for i in range(len(form_letter)):
+        with open(f"./Output/ReadyToSend/{name}_letter.txt", "a") as invite:
+            invite.write(form_letter[i])
