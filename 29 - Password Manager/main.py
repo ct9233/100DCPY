3f28bbb29d428afd521ff2f82415dddac7be703a
Chris Twaddell <ct9233@gmail.com>
2021-02-23 20:39:39 -0800

    Password Generation

--- 29 - Password Manager/main.py
+++ 29 - Password Manager/main.py
@@ -1,9 +1,27 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 WINDOW_COLOR = "#3d4d80"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+    password_letters = [choice(letters) for i in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for i in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for i in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_input.delete(0, END)
+    password_input.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -54,7 +72,7 @@ password_text.grid(row=3, column=0, sticky=E)
 password_input = Entry(width=21, bg=WINDOW_COLOR)
 password_input.grid(row=3, column=1, sticky=EW)
 
-generate_button = Button(text="Generate Password", bg=WINDOW_COLOR)
+generate_button = Button(text="Generate Password", bg=WINDOW_COLOR, command=generate_password)
 generate_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, bg=WINDOW_COLOR, command=save)
