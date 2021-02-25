05fa80353079c44a151aa07a798a32bf8a8a70e3
Chris Twaddell <ct9233@gmail.com>
2021-02-24 17:41:13 -0800

    Error Handling

--- 26 - NATO Alphabet Project/main.py
+++ 26 - NATO Alphabet Project/main.py
@@ -3,8 +3,15 @@ import pandas
 code_words_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 code_words = {row.letter:row.code for (index, row) in code_words_df.iterrows()}
 
-user_word = input("Enter a word to have spelled in the NATO phonetic alphabet: ")
+def generate_code():
+    user_word = input("Enter a word to have spelled in the NATO phonetic alphabet: ")
 
-code_reply = [code_words[f"{letter}"] for letter in list(user_word.upper())]
+    try:
+        code_reply = [code_words[f"{letter}"] for letter in list(user_word.upper())]
+    except KeyError:
+        print("Sorry, only letters of the alphabet please.")
+        generate_code()
+    else:
+        print(code_reply)
 
-print(code_reply)
+generate_code()
