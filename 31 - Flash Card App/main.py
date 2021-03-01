7482982cc8b0542c8da433d50fd21773e8415384
Chris Twaddell <ct9233@gmail.com>
2021-02-28 17:50:25 -0800

    Card Flipping

--- 31 - Flash Card App/main.py
+++ 31 - Flash Card App/main.py
@@ -5,11 +5,23 @@ import pandas
 BACKGROUND_COLOR = "#b1ddc6"
 data = pandas.read_csv("data/french_words.csv")
 word_list = data.to_dict(orient="records")
+current_word = {}
+
 
 def new_word():
-    random_word = random.choice(word_list)
-    canvas.itemconfig(card_front_title, text="French")
-    canvas.itemconfig(card_front_word, text=random_word["French"])
+    global current_word, flip_timer
+    window.after_cancel(flip_timer)
+    current_word = random.choice(word_list)
+    canvas.itemconfig(card_display, image=card_front_img)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_word["French"], fill="black")
+    flip_timer = window.after(3000, flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_display, image=card_back_img)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_word["English"], fill="white")
+
 
 # ----------------------------- UI Setup -----------------------------  #
 
@@ -17,11 +29,14 @@ window = Tk()
 window.title("Flashcard App")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, flip_card)
+
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
-card_front_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
-card_front_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+card_back_img = PhotoImage(file="images/card_back.png")
+card_display = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
