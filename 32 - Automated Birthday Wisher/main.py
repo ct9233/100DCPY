76c9085801a8117a7898bd75e8ed0989f8ead708
Chris Twaddell <ct9233@gmail.com>
2021-03-01 19:19:52 -0800

    Date Match Birthday and Create Letter

--- 32 - Automated Birthday Wisher/main.py
+++ 32 - Automated Birthday Wisher/main.py
@@ -1,6 +1,7 @@
 import smtplib
 import datetime
 import pandas
+import random
 
 EMAIL = "email@server.com"
 PASSWORD = "1234PASS"
@@ -9,7 +10,14 @@ now = datetime.datetime.now()
 today = (now.month, now.day)
 
 data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrrows()}
 
+if today in birthdays_dict:
+    birthday_match = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents.replace("[NAME]", birthday_match["name"])
 
 
 with smtplib.SMTP("smtp.gmail.com") as connection:
