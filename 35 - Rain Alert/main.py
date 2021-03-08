81d64dfa88b15e163a03f746ceb9fe7e047bf624
Chris Twaddell <ct9233@gmail.com>
2021-03-07 17:50:33 -0800

    API Key Security with dotenv

--- 35 - Rain Alert/main.py
+++ 35 - Rain Alert/main.py
@@ -1,14 +1,19 @@
 import requests
 import os
+from dotenv import load_dotenv
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-api_key = os.environ.get("OWM_API_KEY")
+load_dotenv("C:/data/.env")
+api_key = os.getenv("OWM_API_KEY")
 
 weather_params = {
     "lat":  32.715736,
     "lon":  -117.161087,
     "appid": api_key,
+    "exclude": "current,minutely,daily",
 }
 
 response = requests.get(OWM_Endpoint, params=weather_params)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+print(weather_data)
