151ce1bde1a92025da40ba6c45aa07538c1747c5
Chris Twaddell <ct9233@gmail.com>
2021-03-08 17:14:27 -0800

    Send SMS with Twilio API

--- 35 - Rain Alert/main.py
+++ 35 - Rain Alert/main.py
@@ -1,14 +1,19 @@
 import requests
 import os
 from dotenv import load_dotenv
+from twilio.rest import Client
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 load_dotenv("C:/data/.env")
 api_key = os.getenv("OWM_API_KEY")
+account_sid = os.getenv("TWI_ACC_SID")
+auth_token = os.getenv("TWI_AUTH_TOK")
+twilio_from = os.getenv("TWI_FROM_NUM")
+twilio_to = os.getenv("TWI_TO_NUM")
 
 weather_params = {
-    "lat":  32.715736,
-    "lon":  -117.161087,
+    "lat": 43.615021,
+    "lon": -116.202316,
     "appid": api_key,
     "exclude": "current,minutely,daily",
 }
@@ -24,7 +29,15 @@ for hour_data in weather_slice:
     condition_code = hour_data["weather"][0]["id"]
     if int(condition_code) < 700:
         will_rain = True
-        
+
 if will_rain:
-    print("Bring an umbrella!")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔",
+        from_=f"{twilio_from}",
+        to=f"{twilio_to}"
+    )
+    print(message.status)
+    # print("Bring an umbrella!")
 # print(weather_data["hourly"][0]["weather"][0]["id"])
