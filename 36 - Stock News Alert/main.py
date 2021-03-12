295cbd3d9299cc126ee8c9df4b0bc7b718dbef60
Chris Twaddell <ct9233@gmail.com>
2021-03-11 17:27:25 -0800

    Send SMS with Twilio API

--- 36 - Stock News Alert/main.py
+++ 36 - Stock News Alert/main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import requests
 from datetime import datetime, timedelta
 import json
+from twilio.rest import Client
 
 
 load_dotenv("C:/data/.env")
@@ -13,6 +14,10 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 stock_api = os.getenv("ALPHA_VANT_API")
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 news_api = os.getenv("NEWS_API")
+account_sid = os.getenv("TWI_ACC_SID")
+auth_token = os.getenv("TWI_AUTH_TOK")
+twilio_from = os.getenv("TWI_FROM_NUM")
+twilio_to = os.getenv("TWI_TO_NUM")
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -32,7 +37,7 @@ price_change = abs(float(yesterdays_close) - float(day_before_yesterday_close))
 percent_change = price_change/float(yesterdays_close) * 100
 print(percent_change)
 
-if percent_change > 5:
+if percent_change > 4:
     news_params = {
         "apiKey": news_api,
         "qInTitle": COMPANY_NAME,
@@ -40,6 +45,18 @@ if percent_change > 5:
 
     news_response = requests.get(NEWS_ENDPOINT, params=news_params)
     news_response.raise_for_status()
-    news_data = news_response.json()['articles'][:3]
+    news_data = news_response.json()['articles']
+    if len(news_data) > 3:
+        news_data = news_data[:3]
 
-    print(news_data)
+    formatted_articles = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in news_data]
+    print(formatted_articles)
+
+    client = Client(account_sid, auth_token)
+    for article in formatted_articles:
+        message = client.messages \
+            .create(
+            body=article,
+            from_=f"{twilio_from}",
+            to=f"{twilio_to}"
+        )
