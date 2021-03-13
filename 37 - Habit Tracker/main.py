258190cc336e55b7149dbdf26ab5fbbe6690f86a
Chris Twaddell <ct9233@gmail.com>
2021-03-12 17:15:36 -0800

    Pixela Graph Setup

--- 37 - Habit Tracker/main.py
+++ 37 - Habit Tracker/main.py
@@ -17,3 +17,20 @@ user_params = {
 
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{username}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Coding Graph",
+    "unit": "hours",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": token,
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
