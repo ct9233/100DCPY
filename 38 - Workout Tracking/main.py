42127b6d713f2875e07cf31980d5e48a0ee2f12e
Chris Twaddell <ct9233@gmail.com>
2021-03-17 18:16:52 -0700

    Adding Data with Sheety API

--- 38 - Workout Tracking/main.py
+++ 38 - Workout Tracking/main.py
@@ -13,7 +13,8 @@ headers = {
     "x-app-key": nutritionix_api,
 }
 
-EXERCISE_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_endpoint = os.getenv("SHEETY_WKOUT_ENDPOINT")
 
 user_exercises = input("Which excercises did you complete? ")
 
@@ -25,14 +26,24 @@ exercise_config = {
     "age": os.getenv("AGE"),
 }
 
-response = requests.post(EXERCISE_ENDPOINT, json=exercise_config, headers=headers)
+response = requests.post(exercise_endpoint, json=exercise_config, headers=headers)
 response.raise_for_status()
 exercise_data = response.json()
-
-print(exercise_data)
 
 today_date = datetime.now().strftime("%d/%m/%Y")
 now_time = datetime.now().strftime("%X")
 
-print(today_date)
-print(now_time)
+for exercise in exercise_data["exercises"]:
+    sheety_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+
+sheety_response = requests.post(sheety_endpoint, json=sheety_inputs)
+
+print(sheety_response.text)
