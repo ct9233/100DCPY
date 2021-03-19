483e731ce1bb66c9f8d5034cb45afbae455eb548
Chris Twaddell <ct9233@gmail.com>
2021-03-18 19:31:05 -0700

    API Setups

--- 39 - Flight Deal Finder/data_manager.py
+++ 39 - Flight Deal Finder/data_manager.py
@@ -1,3 +1,11 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv("C:/data/.env")
+
+sheety_endpoint = os.getenv("SHEETY_FLIGHT_PRICES_ENDPOINT")
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     pass
--- 39 - Flight Deal Finder/flight_search.py
+++ 39 - Flight Deal Finder/flight_search.py
@@ -1,3 +1,13 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv("C:/data/.env")
+
+tequila_main_endpoint = "https://tequila-api.kiwi.com"
+tequila_locations_query = "/locations/query"
+tequila_api_key = os.getenv("KWTQ_API_KEY")
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     pass
