15f2ad225a221eb5f6434371176d1824e2c4cd30
Chris Twaddell <ct9233@gmail.com>
2021-03-19 18:12:16 -0700

    Write Test Data to Sheet

--- 39 - Flight Deal Finder/flight_search.py
+++ 39 - Flight Deal Finder/flight_search.py
@@ -10,4 +10,6 @@ tequila_api_key = os.getenv("KWTQ_API_KEY")
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    def get_destination_code(self, city_name):
+        code = "TESTING"
+        return code
--- 39 - Flight Deal Finder/main.py
+++ 39 - Flight Deal Finder/main.py
@@ -2,3 +2,14 @@ from data_manager import DataManager
 from flight_data import FlightData
 from flight_search import FlightSearch
 from notification_manager import NotificationManager
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+
+if sheet_data[0]["iataCode"] == "":
+    flight_search = FlightSearch()
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
