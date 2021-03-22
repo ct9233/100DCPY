12d467a2ef4652da28f6650fa04d0b3643a5cacc
Chris Twaddell <ct9233@gmail.com>
2021-03-21 18:16:20 -0700

    IATA Codes from API to Sheet

--- 39 - Flight Deal Finder/flight_search.py
+++ 39 - Flight Deal Finder/flight_search.py
@@ -5,11 +5,15 @@ import requests
 load_dotenv("C:/data/.env")
 
 tequila_main_endpoint = "https://tequila-api.kiwi.com"
-tequila_locations_query = "/locations/query"
 tequila_api_key = os.getenv("KWTQ_API_KEY")
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     def get_destination_code(self, city_name):
-        code = "TESTING"
+        location_endpoint = f"{tequila_main_endpoint}/locations/query"
+        headers = {"apikey": tequila_api_key}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code
--- 39 - Flight Deal Finder/main.py
+++ 39 - Flight Deal Finder/main.py
@@ -6,9 +6,9 @@ from notification_manager import NotificationManager
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 
-if sheet_data[0]["iataCode"] == "":
-    flight_search = FlightSearch()
-    for row in sheet_data:
+for row in sheet_data:
+    if row["iataCode"] == "":
+        flight_search = FlightSearch()
         row["iataCode"] = flight_search.get_destination_code(row["city"])
 
     data_manager.destination_data = sheet_data
