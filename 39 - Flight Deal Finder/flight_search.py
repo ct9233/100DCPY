d1c749bd5ae58c53d327e4e2b385643c4bebadda
Chris Twaddell <ct9233@gmail.com>
2021-03-22 19:53:03 -0700

    Flight Check Data

--- 39 - Flight Deal Finder/flight_search.py
+++ 39 - Flight Deal Finder/flight_search.py
@@ -17,3 +17,18 @@ class FlightSearch:
         results = response.json()["locations"]
         code = results[0]["code"]
         return code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {"apikey": tequila_api_key}
+        query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 21,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 1,
+            "curr": "USD"
+        }
--- 39 - Flight Deal Finder/main.py
+++ 39 - Flight Deal Finder/main.py
@@ -1,15 +1,20 @@
 from data_manager import DataManager
-from flight_data import FlightData
 from flight_search import FlightSearch
 from notification_manager import NotificationManager
+from datetime import datetime, timedelta
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
+
+origin_city_iata = "ORD"
 
 for row in sheet_data:
     if row["iataCode"] == "":
         flight_search = FlightSearch()
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_now = datetime.now() + timedelta(days=(6 * 30))
+
