49ae16927131ab84a494d2111d48fe1616c3f09b
Chris Twaddell <ct9233@gmail.com>
2021-03-23 19:28:09 -0700

    Search Sheet Codes for Prices

--- 39 - Flight Deal Finder/main.py
+++ 39 - Flight Deal Finder/main.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
 
 origin_city_iata = "ORD"
 
@@ -18,3 +19,10 @@ for row in sheet_data:
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_now = datetime.now() + timedelta(days=(6 * 30))
 
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        origin_city_iata,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_now
+    )
