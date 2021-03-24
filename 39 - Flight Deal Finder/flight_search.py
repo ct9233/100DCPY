7172030610715e4ce6ca9db578e654a11a7b4a17
Chris Twaddell <ct9233@gmail.com>
2021-03-23 18:11:04 -0700

    Flights and Price Data

--- 39 - Flight Deal Finder/flight_search.py
+++ 39 - Flight Deal Finder/flight_search.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import requests
+from flight_data import FlightData
 
 load_dotenv("C:/data/.env")
 
@@ -29,6 +30,30 @@ class FlightSearch:
             "nights_in_dst_to": 21,
             "flight_type": "round",
             "one_for_city": 1,
-            "max_stopovers": 1,
+            "max_stopovers": 0,
             "curr": "USD"
         }
+
+        response = requests.get(
+            url=f"{tequila_main_endpoint}/v2/search",
+            headers=headers,
+            params=query,
+        )
+
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            depart_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0],
+        )
+        print(f"{flight_data.destination_city}: ${flight_data.price}")
+        return flight_data
