eba081702c29ec4dd406564d1bcb7eb4c5f5479a
Chris Twaddell <ct9233@gmail.com>
2021-03-24 19:38:38 -0700

    Notification Manager Class

--- 39 - Flight Deal Finder/notification_manager.py
+++ 39 - Flight Deal Finder/notification_manager.py
@@ -1,3 +1,23 @@
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+load_dotenv("C:/data/.env")
+
+account_sid = os.getenv("TWI_ACC_SID")
+auth_token = os.getenv("TWI_AUTH_TOK")
+twilio_from = os.getenv("TWI_FROM_NUM")
+twilio_to = os.getenv("TWI_TO_NUM")
+
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        self.client = Client(account_sid, auth_token)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from=twilio_from,
+            to=twilio_to
+        )
+        print(message.sid)
