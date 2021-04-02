5cc3ea05b31916973b402e4cc25cfffa1456d56f
Chris Twaddell <ct9233@gmail.com>
2021-04-01 18:19:34 -0700

    Refactored List and File Output

--- 45 - 100 Movies/main.py
+++ 45 - 100 Movies/main.py
@@ -10,15 +10,15 @@ soup = BeautifulSoup(movie_web_page, "html.parser")
 tags_list = soup.find_all(name="h3", class_="card-title")
 movie_list = [(item.getText()) for item in tags_list]
 
-ranking_list = ''
+ranking_list = []
 
 for movie in movie_list:
-    movie_rank = movie.strip()
-    if movie_rank[0] != "C":
-        ranking_list += f"{movie_rank} \n"
+    movie_ranked = movie.strip()
+    ranking_list.append(movie_ranked)
 
 with open("movies.txt", "w", encoding="utf-16") as file:
-    file.write(ranking_list)
+    for movie in ranking_list[:-1]:
+        file.write(f"{movie}\n")
 
 
 
