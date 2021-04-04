dbb832915b8d9f037641a77be6bcb02c5d4ec956
Chris Twaddell <ct9233@gmail.com>
2021-04-03 19:24:21 -0700

    Spotify Authentication

--- 46 - Music TIme Machine/main.py
+++ 46 - Music TIme Machine/main.py
@@ -1,5 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+load_dotenv("C:/data/.env")
+sptfy_client_id = os.getenv("SPTFY_ID")
+sptfy_client_secret = os.getenv("SPTFY_SECRET")
 
 date = input("Which year do you want to retrieve music from? \nEnter date in the format YYYY-MM-DD: ")
 
@@ -9,3 +17,15 @@ response.raise_for_status()
 soup = BeautifulSoup(response.text, "html.parser")
 song_names_spans = soup.find_all("span", class_="chart-element__information__song")
 song_names = [song.getText() for song in song_names_spans]
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="https://example.com",
+        client_id=sptfy_client_id,
+        client_secret=sptfy_client_secret,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
