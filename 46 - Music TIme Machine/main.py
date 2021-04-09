90ff2a758abc8c56c6fda114e4335c444c48caad
Chris Twaddell <ct9233@gmail.com>
2021-04-08 19:14:57 -0700

    Search Spotify for Songs

--- 46 - Music TIme Machine/main.py
+++ 46 - Music TIme Machine/main.py
@@ -11,6 +11,7 @@ sptfy_client_secret = os.getenv("SPTFY_SECRET")
 
 date = input("Which year do you want to retrieve music from? \nEnter date in the format YYYY-MM-DD: ")
 
+# Scraping Billboard for songs
 response = requests.get(f"https://billboard.com/charts/hot-100/{date}")
 response.raise_for_status()
 
@@ -18,6 +19,7 @@ soup = BeautifulSoup(response.text, "html.parser")
 song_names_spans = soup.find_all("span", class_="chart-element__information__song")
 song_names = [song.getText() for song in song_names_spans]
 
+# Spotify authentication
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
@@ -29,3 +31,16 @@ sp = spotipy.Spotify(
     )
 )
 user_id = sp.current_user()["id"]
+
+# Search Spotify for song titles
+song_uris = []
+year = date.split("-")[0]
+
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} not found in Spotify.")
