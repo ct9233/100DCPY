dcad9980e97e6c95d4797652533c008637473a79
Chris Twaddell <ct9233@gmail.com>
2021-04-15 19:56:19 -0700

    Click Highest Affordable Upgrade

--- 48 - Cookie Clicker Project/main.py
+++ 48 - Cookie Clicker Project/main.py
@@ -17,9 +17,23 @@ while True:
     if time.time() >= upgrade_check_time:
         player_money = int(driver.find_element_by_id("money").text.replace(",", ""))
         upgrade_items = driver.find_elements_by_css_selector("#store b")
-        
+        upgrade_dict = {}
+        costs = []
+
         for item in upgrade_items:
-            print(item.text)
+            if item.text != "":
+                name = item.text.split("-")[0].strip()
+                cost = int(item.text.split("-")[1].strip().replace(",", ""))
+                costs.append(cost)
+            upgrade_dict[cost] = name
+            
+        costs.sort(reverse=True)
+
+        for cost in costs:
+            if cost < player_money:
+                driver.find_element_by_id(f"buy{upgrade_dict[cost]}").click()
+                break
+
         upgrade_check_time = time.time() + 5
 
     if time.time() > game_time_limit:
