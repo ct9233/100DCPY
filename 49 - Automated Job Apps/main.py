3a435288cd402ad4d1684937a92a04c273ebfab8
Chris Twaddell <ct9233@gmail.com>
2021-04-16 18:00:22 -0700

    Selenium and Site Setup

---
+++ 49 - Automated Job Apps/main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv("C:/data/.env")
+account_email = os.getenv("LINKEDIN_ACCT")
+account_password = os.getenv("LINKEDIN_PASS")
+
+chrome_driver_path = "/data/chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver.get("https://www.linkedin.com/jobs/search/?currentJobId=2485157619&f_AL=true&f_CR=103644278&f_E=2%2C1&f_TPR=r604800&f_WRA=true&keywords=developer&sortBy=DD")
+
+sign_in_button = driver.find_element_by_link_text("Sign in")
+sign_in_button.click()
+
+time.sleep()
