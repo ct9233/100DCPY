4a210e72233fc96586ed5e0e1eca29f5d91418a8
Chris Twaddell <ct9233@gmail.com>
2021-05-06 19:33:53 -0700

    Instagram Login

--- 52 - Instagram Follower/main.py
+++ 52 - Instagram Follower/main.py
@@ -1,10 +1,13 @@
 import os
 from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 load_dotenv("C:/data/.env")
 
 CHROME_DRIVER_PATH = "/data/chromedriver"
+FOLLOW_ACCOUNT = "chefsteps"
 USERNAME = os.getenv("IG_USERNAME")
 PASSWORD = os.getenv("IG_PASSWORD")
 
@@ -13,7 +16,17 @@ class InstaFollower:
         self.driver = webdriver.Chrome(executable_path=path)
 
     def login(self):
-        pass
+        self.driver.get("https://www.instagram.com/accounts/login/")
+        time.sleep(5)
+
+        username = self.driver.find_element_by_name("username")
+        password = self.driver.find_element_by_name("password")
+
+        username.send_keys(USERNAME)
+        password.send_keys(PASSWORD)
+
+        time.sleep(2)
+        password.send_keys(Keys.ENTER)
 
     def find_followers(self):
         pass
