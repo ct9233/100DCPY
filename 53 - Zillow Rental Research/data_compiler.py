26d5b7a86b960f61a58f1ffd332bbb2b6ad4e29b
Chris Twaddell <ct9233@gmail.com>
2021-05-13 17:52:56 -0700

    Adding Data to Sheet

--- 53 - Zillow Rental Research/data_compiler.py
+++ 53 - Zillow Rental Research/data_compiler.py
@@ -12,3 +12,16 @@ FORM_URL = os.getenv("APT_FORM_URL")
 class DataCompiler():
     def __init__(self):
         self.driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+
+    def write_data(self, link, price, address):
+        self.driver.get(FORM_URL)
+        time.sleep(2)
+        address_input = self.driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        price_input = self.driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        link_input = self.driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        submit_btn = self.driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span')
+
+        address_input.send_keys(address)
+        price_input.send_keys(price)
+        link_input.send_keys(link)
+        submit_btn.click()
--- 53 - Zillow Rental Research/main.py
+++ 53 - Zillow Rental Research/main.py
@@ -1,2 +1,14 @@
 from apartment_research import ApartmentResearch
 from data_compiler import DataCompiler
+
+apartment_research = ApartmentResearch()
+
+apartment_research.get_data()
+links = apartment_research.links
+prices = apartment_research.prices
+addresses = apartment_research.addresses
+
+data_compiler = DataCompiler()
+
+for i in range(len(links)):
+    data_compiler.write_data(addresses[i], prices[i], links[i])
