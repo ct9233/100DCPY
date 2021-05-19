5f9c483c4fbfc4efc127a8f51faf1bbcddd1aa08
Chris Twaddell <ct9233@gmail.com>
2021-05-18 19:16:21 -0700

    Get Posts and Populate Titles

--- 56 - Blog Templating/main.py
+++ 56 - Blog Templating/main.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+
+def get_posts():
+    blog_api_url = "https://api.npoint.io/ed99320662742443cc5b"
+    response = requests.get(blog_api_url)
+    return response.json()
+
+
+@app.route("/")
 def home():
-    return render_template("index.html")
+    all_posts = get_posts()
+    return render_template("index.html", posts=all_posts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
