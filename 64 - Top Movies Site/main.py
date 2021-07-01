4b02598b05bd3afa5b89981f4947bdabe34ee7bc
Chris Twaddell <ct9233@gmail.com>
2021-06-30 20:54:48 -0700

    Add New Movie Data

--- 64 - Top Movies Site/main.py
+++ 64 - Top Movies Site/main.py
@@ -11,6 +11,8 @@ from dotenv import load_dotenv
 
 load_dotenv("C:/data/.env")
 TMDB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+TMDB_INFO_URL = "https://api.themoviedb.org/3/movie"
+TMDB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 TMDB_API = os.getenv("TMDB_API")
 
 app = Flask(__name__)
@@ -89,6 +91,23 @@ def add_movie():
         return render_template("select.html", options=data)
 
     return render_template("add.html", form=form)
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{TMDB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": TMDB_API})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{TMDB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+    db.session.add(new_movie)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run(debug=True)
